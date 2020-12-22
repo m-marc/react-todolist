540f4e54dd98c05e85c3fd03b02815ed7840b3f5
m-marc <marchenkovmaxim@gmail.com>
2020-12-22 19:24:08 +0300

    Updates

---
+++ precache-manifest.419f86c1cff82e254a78594d695b7030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f32518721499308369e562b476da3",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "f493e48f6c465be429db",
+    "url": "/react-todolist/static/css/main.d0b8a73b.chunk.css"
+  },
+  {
+    "revision": "5f354a444428d070bcb4",
+    "url": "/react-todolist/static/js/2.8c106c06.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/react-todolist/static/js/2.8c106c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f493e48f6c465be429db",
+    "url": "/react-todolist/static/js/main.90b72e7c.chunk.js"
+  },
+  {
+    "revision": "21e483b4943fc3e99aca",
+    "url": "/react-todolist/static/js/runtime-main.92dbfbf8.js"
+  }
+]);
